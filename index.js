9d9b4192d9148a120ef34997208577f8bb211370
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-09 18:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!";
-  } else if (string.toUpperCase === string) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } else {
     return "I love you, too.";
